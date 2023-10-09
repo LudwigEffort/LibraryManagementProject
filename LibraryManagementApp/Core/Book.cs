6e82ec54936ddf9955dcceca914956ef99b7f428
Trovato Francesco <francescotrov@outlook.com>
2023-10-09 16:24:55 +0200

    function for search by...

--- LibraryManagementApp/Core/Book.cs
+++ LibraryManagementApp/Core/Book.cs
@@ -3,7 +3,8 @@ namespace LibraryManagementApp.Core
 {
     class Book : Document
     {
-        string ISBN, Pages;
+        string ISBN {get;set;}
+        string Pages{get;set;}
 
         public Book(string title, string year, string genre, string location, bool status, List<string> authors, string isbn, string pages) : base(title, year, genre, location, status, authors)
         {
